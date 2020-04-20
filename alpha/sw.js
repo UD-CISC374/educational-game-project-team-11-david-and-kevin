e56c0210d3b85c7b7fa1a7371eb03ae4534c4798
klmjr <klmjr@udel.edu>
2020-04-19 23:58:59 -0400

    Updates

---
+++ alpha/sw.js
@@ -0,0 +1,11 @@
+importScripts("/educational-phaser-game-template/alphaprecache-manifest.ed3c103ffe1e42f5e7284454488134a5.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/**
+ * You should only modify this, if you know what you are doing.
+ * This phaser template is using workbox (https://developers.google.com/web/tools/workbox/)
+ * to precache all assets.
+ * It uses the InjectManifest function from 'workbox-webpack-plugin' inside
+ * webpack/webpack.common.js
+ */
+workbox.precaching.precacheAndRoute(__precacheManifest)
+
---
+++ precache-manifest.a199c9c6191a61f222497e8b78cc970a.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7087d5d91321d575b038366ef9f2407",
+    "url": "assets/citations.md"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "assets/icons/favicon.ico"
+  },
+  {
+    "revision": "8ab30def0ad109a97117bd62db942d75",
+    "url": "assets/icons/icons-16.png"
+  },
+  {
+    "revision": "feee71db2bb3ba5d11b26658baf97783",
+    "url": "assets/icons/icons-192.png"
+  },
+  {
+    "revision": "cb71713be71d0b8820c7ea6af9c5ddc6",
+    "url": "assets/icons/icons-512.png"
+  },
+  {
+    "revision": "200ce541edfaa71be93a1041736d1fee",
+    "url": "assets/icons/icons8-game-controller-16.png"
+  },
+  {
+    "revision": "dc13785b00cfa7278d41c7f109a507e3",
+    "url": "assets/images/background.png"
+  },
+  {
+    "revision": "426c64762368c2e78f65f511ad0edbb8",
+    "url": "assets/spritesheets/AE.png"
+  },
+  {
+    "revision": "96e4d0c0283eba0eee45713c1cbcd786",
+    "url": "assets/spritesheets/CS.png"
+  },
+  {
+    "revision": "2c4b83a126c6a0ed5fde6989e4daf6a0",
+    "url": "assets/tilemaps/alpha-map.json"
+  },
+  {
+    "revision": "1e27e93bcb2fd9b82661b8054e31a700",
+    "url": "assets/tilesets/BasicMapTiles.png"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "favicon.ico"
+  },
+  {
+    "revision": "a8e23cfa463f4c38eba8cd539f084e1a",
+    "url": "index.html"
+  },
+  {
+    "revision": "ff3bbf5fe53620359d10",
+    "url": "main.f715d114c3db6f96ef6c.bundle.js"
+  },
+  {
+    "revision": "003187cab0ecf7a20e343e2ba6db4a34",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "sw.js"
+  },
+  {
+    "revision": "5e69403aa42e950fedff",
+    "url": "vendors.0805bb80903235d40eaf.bundle.js"
+  }
+]);
