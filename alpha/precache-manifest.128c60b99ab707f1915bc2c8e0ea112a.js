ed1b7bbe4e59de6c2b9c78a6d0978546c9b87008
klmjr <klmjr@udel.edu>
2020-04-21 13:15:13 -0400

    Updates

---
+++ alpha/precache-manifest.128c60b99ab707f1915bc2c8e0ea112a.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7087d5d91321d575b038366ef9f2407",
+    "url": "/educational-game-project-team-11-david-and-kevin/alphaassets/citations.md"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "/educational-game-project-team-11-david-and-kevin/alphaassets/icons/favicon.ico"
+  },
+  {
+    "revision": "8ab30def0ad109a97117bd62db942d75",
+    "url": "/educational-game-project-team-11-david-and-kevin/alphaassets/icons/icons-16.png"
+  },
+  {
+    "revision": "feee71db2bb3ba5d11b26658baf97783",
+    "url": "/educational-game-project-team-11-david-and-kevin/alphaassets/icons/icons-192.png"
+  },
+  {
+    "revision": "cb71713be71d0b8820c7ea6af9c5ddc6",
+    "url": "/educational-game-project-team-11-david-and-kevin/alphaassets/icons/icons-512.png"
+  },
+  {
+    "revision": "200ce541edfaa71be93a1041736d1fee",
+    "url": "/educational-game-project-team-11-david-and-kevin/alphaassets/icons/icons8-game-controller-16.png"
+  },
+  {
+    "revision": "5006d21ea5bdbc2beb4e7ce1685db825",
+    "url": "/educational-game-project-team-11-david-and-kevin/alphaassets/images/Mountain.png"
+  },
+  {
+    "revision": "f7f5a097e97f9f6231cda9566789a46f",
+    "url": "/educational-game-project-team-11-david-and-kevin/alphaassets/images/Tree.png"
+  },
+  {
+    "revision": "e43d910498fa80bab65d72445dcd7e73",
+    "url": "/educational-game-project-team-11-david-and-kevin/alphaassets/images/alpha-map-background.png"
+  },
+  {
+    "revision": "314f0c5e054fd70812203fd11c7f9d15",
+    "url": "/educational-game-project-team-11-david-and-kevin/alphaassets/images/bucket.png"
+  },
+  {
+    "revision": "1189e723e82fdbad00f278b2968f2abc",
+    "url": "/educational-game-project-team-11-david-and-kevin/alphaassets/images/tiles01.png"
+  },
+  {
+    "revision": "415f2b2a069e511d052658c239a41158",
+    "url": "/educational-game-project-team-11-david-and-kevin/alphaassets/images/tiles02.png"
+  },
+  {
+    "revision": "fb3e9d2e3d885c9a3b40782ce119c162",
+    "url": "/educational-game-project-team-11-david-and-kevin/alphaassets/images/tiles03.png"
+  },
+  {
+    "revision": "8e946caca7eac821d76dc6796c599725",
+    "url": "/educational-game-project-team-11-david-and-kevin/alphaassets/images/tiles04.png"
+  },
+  {
+    "revision": "8e946caca7eac821d76dc6796c599725",
+    "url": "/educational-game-project-team-11-david-and-kevin/alphaassets/images/tiles05.png"
+  },
+  {
+    "revision": "d815b6ca0a27753177293ce9a174e650",
+    "url": "/educational-game-project-team-11-david-and-kevin/alphaassets/images/tiles06.png"
+  },
+  {
+    "revision": "45e0c49fc450e18fb2ec9ebfdfeb3dc4",
+    "url": "/educational-game-project-team-11-david-and-kevin/alphaassets/images/tiles07.png"
+  },
+  {
+    "revision": "58b927db510d4aeb2b84fdbbea344e6f",
+    "url": "/educational-game-project-team-11-david-and-kevin/alphaassets/images/tiles08.png"
+  },
+  {
+    "revision": "0e33eec01d70ecd0e4aa0f530275837e",
+    "url": "/educational-game-project-team-11-david-and-kevin/alphaassets/images/tiles09.png"
+  },
+  {
+    "revision": "dfb9bbbf2045b8a31cc236298fbac1b2",
+    "url": "/educational-game-project-team-11-david-and-kevin/alphaassets/images/tiles10.png"
+  },
+  {
+    "revision": "f258441fa8f9aa5929b5233fd327cf9b",
+    "url": "/educational-game-project-team-11-david-and-kevin/alphaassets/images/tiles11.png"
+  },
+  {
+    "revision": "426c64762368c2e78f65f511ad0edbb8",
+    "url": "/educational-game-project-team-11-david-and-kevin/alphaassets/spritesheets/AE.png"
+  },
+  {
+    "revision": "96e4d0c0283eba0eee45713c1cbcd786",
+    "url": "/educational-game-project-team-11-david-and-kevin/alphaassets/spritesheets/CS.png"
+  },
+  {
+    "revision": "743745d7bb43111c304e486adfb81afa",
+    "url": "/educational-game-project-team-11-david-and-kevin/alphaassets/spritesheets/seedsandplants.png"
+  },
+  {
+    "revision": "723454824e083d81336c51b782aed6b3",
+    "url": "/educational-game-project-team-11-david-and-kevin/alphaassets/spritesheets/tubing.png"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "/educational-game-project-team-11-david-and-kevin/alphafavicon.ico"
+  },
+  {
+    "revision": "0e84e13f32f19377e8f1",
+    "url": "/educational-game-project-team-11-david-and-kevin/alphagen.main.4969d49fe9ad4a2e4c1f.bundle.js"
+  },
+  {
+    "revision": "5e69403aa42e950fedff",
+    "url": "/educational-game-project-team-11-david-and-kevin/alphagen.vendors.0805bb80903235d40eaf.bundle.js"
+  },
+  {
+    "revision": "39127a485bccae07ff602868c927f4f2",
+    "url": "/educational-game-project-team-11-david-and-kevin/alphaindex.html"
+  },
+  {
+    "revision": "2820266d63e93bb9239043f6277350bf",
+    "url": "/educational-game-project-team-11-david-and-kevin/alphamanifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/educational-game-project-team-11-david-and-kevin/alphasw.js"
+  }
+]);
--- alpha/sw.js
+++ alpha/sw.js
@@ -1,4 +1,4 @@
-importScripts("/educational-phaser-game-template/alphaprecache-manifest.1ac7980150076460a701f9873e8a5434.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/educational-game-project-team-11-david-and-kevin/alphaprecache-manifest.128c60b99ab707f1915bc2c8e0ea112a.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /**
  * You should only modify this, if you know what you are doing.
