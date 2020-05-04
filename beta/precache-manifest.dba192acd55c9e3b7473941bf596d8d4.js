2315ad10520c53c28ab0856893c2888f256b6d77
klmjr <klmjr@udel.edu>
2020-05-03 23:56:04 -0400

    Updates

---
+++ beta/precache-manifest.dba192acd55c9e3b7473941bf596d8d4.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7087d5d91321d575b038366ef9f2407",
+    "url": "/educational-game-project-team-11-david-and-kevin/betaassets/citations.md"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "/educational-game-project-team-11-david-and-kevin/betaassets/icons/favicon.ico"
+  },
+  {
+    "revision": "8ab30def0ad109a97117bd62db942d75",
+    "url": "/educational-game-project-team-11-david-and-kevin/betaassets/icons/icons-16.png"
+  },
+  {
+    "revision": "feee71db2bb3ba5d11b26658baf97783",
+    "url": "/educational-game-project-team-11-david-and-kevin/betaassets/icons/icons-192.png"
+  },
+  {
+    "revision": "cb71713be71d0b8820c7ea6af9c5ddc6",
+    "url": "/educational-game-project-team-11-david-and-kevin/betaassets/icons/icons-512.png"
+  },
+  {
+    "revision": "200ce541edfaa71be93a1041736d1fee",
+    "url": "/educational-game-project-team-11-david-and-kevin/betaassets/icons/icons8-game-controller-16.png"
+  },
+  {
+    "revision": "5006d21ea5bdbc2beb4e7ce1685db825",
+    "url": "/educational-game-project-team-11-david-and-kevin/betaassets/images/Mountain.png"
+  },
+  {
+    "revision": "758672cac3cadde3cc323c9c920cf5ea",
+    "url": "/educational-game-project-team-11-david-and-kevin/betaassets/images/Tree.png"
+  },
+  {
+    "revision": "e43d910498fa80bab65d72445dcd7e73",
+    "url": "/educational-game-project-team-11-david-and-kevin/betaassets/images/alpha-map-background.png"
+  },
+  {
+    "revision": "793ff4e5e666ff08ecdda38d70b45e6c",
+    "url": "/educational-game-project-team-11-david-and-kevin/betaassets/images/autoMap.json"
+  },
+  {
+    "revision": "314f0c5e054fd70812203fd11c7f9d15",
+    "url": "/educational-game-project-team-11-david-and-kevin/betaassets/images/bucket.png"
+  },
+  {
+    "revision": "c69ed4c292b1300c4f7c998c4459238c",
+    "url": "/educational-game-project-team-11-david-and-kevin/betaassets/images/mappedTiles.png"
+  },
+  {
+    "revision": "d910b52861df9805488a5afb805f2073",
+    "url": "/educational-game-project-team-11-david-and-kevin/betaassets/images/plastic.png"
+  },
+  {
+    "revision": "df3dfc4bb5ebb1a992370a650db90617",
+    "url": "/educational-game-project-team-11-david-and-kevin/betaassets/images/pond.png"
+  },
+  {
+    "revision": "1189e723e82fdbad00f278b2968f2abc",
+    "url": "/educational-game-project-team-11-david-and-kevin/betaassets/images/tiles01.png"
+  },
+  {
+    "revision": "f258441fa8f9aa5929b5233fd327cf9b",
+    "url": "/educational-game-project-team-11-david-and-kevin/betaassets/images/tiles11.png"
+  },
+  {
+    "revision": "cf053af14504ca694cc78b106f9def53",
+    "url": "/educational-game-project-team-11-david-and-kevin/betaassets/images/updatebackground.png"
+  },
+  {
+    "revision": "dcdd1bd9377c2c80a170c42207c4e905",
+    "url": "/educational-game-project-team-11-david-and-kevin/betaassets/images/updatebackground.xcf"
+  },
+  {
+    "revision": "426c64762368c2e78f65f511ad0edbb8",
+    "url": "/educational-game-project-team-11-david-and-kevin/betaassets/spritesheets/AE.png"
+  },
+  {
+    "revision": "1e27e93bcb2fd9b82661b8054e31a700",
+    "url": "/educational-game-project-team-11-david-and-kevin/betaassets/spritesheets/BasicMapTiles.png"
+  },
+  {
+    "revision": "96e4d0c0283eba0eee45713c1cbcd786",
+    "url": "/educational-game-project-team-11-david-and-kevin/betaassets/spritesheets/CS.png"
+  },
+  {
+    "revision": "374ee56ae71305e036e5f5929be2e8ee",
+    "url": "/educational-game-project-team-11-david-and-kevin/betaassets/spritesheets/forrestTile.png"
+  },
+  {
+    "revision": "078e019c7324160e42074f7b26c3a011",
+    "url": "/educational-game-project-team-11-david-and-kevin/betaassets/spritesheets/icons.png"
+  },
+  {
+    "revision": "743745d7bb43111c304e486adfb81afa",
+    "url": "/educational-game-project-team-11-david-and-kevin/betaassets/spritesheets/seedsandplants.png"
+  },
+  {
+    "revision": "28a79e97337144fe01ea5039e4afc5ce",
+    "url": "/educational-game-project-team-11-david-and-kevin/betaassets/spritesheets/tubing.png"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "/educational-game-project-team-11-david-and-kevin/betafavicon.ico"
+  },
+  {
+    "revision": "d57568a62463b171dab3",
+    "url": "/educational-game-project-team-11-david-and-kevin/betagen.main.821a0108449066fe803b.bundle.js"
+  },
+  {
+    "revision": "5e69403aa42e950fedff",
+    "url": "/educational-game-project-team-11-david-and-kevin/betagen.vendors.0805bb80903235d40eaf.bundle.js"
+  },
+  {
+    "revision": "5467439cf2ac7633ea562ee2b70c98dd",
+    "url": "/educational-game-project-team-11-david-and-kevin/betaindex.html"
+  },
+  {
+    "revision": "2820266d63e93bb9239043f6277350bf",
+    "url": "/educational-game-project-team-11-david-and-kevin/betamanifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/educational-game-project-team-11-david-and-kevin/betasw.js"
+  }
+]);
