40c10ec2b5f254ecb9eb91da7c28f12726eeb5ca
klmjr <klmjr@udel.edu>
2020-05-18 23:57:43 -0400

    Updates

---
+++ final/precache-manifest.44ae7d5615360fb1a8d61346f35a4b8c.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7087d5d91321d575b038366ef9f2407",
+    "url": "/educational-game-project-team-11-david-and-kevin/finalassets/citations.md"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "/educational-game-project-team-11-david-and-kevin/finalassets/icons/favicon.ico"
+  },
+  {
+    "revision": "8ab30def0ad109a97117bd62db942d75",
+    "url": "/educational-game-project-team-11-david-and-kevin/finalassets/icons/icons-16.png"
+  },
+  {
+    "revision": "feee71db2bb3ba5d11b26658baf97783",
+    "url": "/educational-game-project-team-11-david-and-kevin/finalassets/icons/icons-192.png"
+  },
+  {
+    "revision": "cb71713be71d0b8820c7ea6af9c5ddc6",
+    "url": "/educational-game-project-team-11-david-and-kevin/finalassets/icons/icons-512.png"
+  },
+  {
+    "revision": "200ce541edfaa71be93a1041736d1fee",
+    "url": "/educational-game-project-team-11-david-and-kevin/finalassets/icons/icons8-game-controller-16.png"
+  },
+  {
+    "revision": "5006d21ea5bdbc2beb4e7ce1685db825",
+    "url": "/educational-game-project-team-11-david-and-kevin/finalassets/images/Mountain.png"
+  },
+  {
+    "revision": "758672cac3cadde3cc323c9c920cf5ea",
+    "url": "/educational-game-project-team-11-david-and-kevin/finalassets/images/Tree.png"
+  },
+  {
+    "revision": "1c936661c75f7a22138caaf5d92e0e1f",
+    "url": "/educational-game-project-team-11-david-and-kevin/finalassets/images/WeaponsAndChest.png"
+  },
+  {
+    "revision": "e43d910498fa80bab65d72445dcd7e73",
+    "url": "/educational-game-project-team-11-david-and-kevin/finalassets/images/alpha-map-background.png"
+  },
+  {
+    "revision": "793ff4e5e666ff08ecdda38d70b45e6c",
+    "url": "/educational-game-project-team-11-david-and-kevin/finalassets/images/autoMap.json"
+  },
+  {
+    "revision": "314f0c5e054fd70812203fd11c7f9d15",
+    "url": "/educational-game-project-team-11-david-and-kevin/finalassets/images/bucket.png"
+  },
+  {
+    "revision": "093f1f8021e2859aaeb25269fe9db73c",
+    "url": "/educational-game-project-team-11-david-and-kevin/finalassets/images/enemy.png"
+  },
+  {
+    "revision": "6ef2ae4af79cce0ad46848611c83f3ce",
+    "url": "/educational-game-project-team-11-david-and-kevin/finalassets/images/mappedTiles.png"
+  },
+  {
+    "revision": "d910b52861df9805488a5afb805f2073",
+    "url": "/educational-game-project-team-11-david-and-kevin/finalassets/images/plastic.png"
+  },
+  {
+    "revision": "df3dfc4bb5ebb1a992370a650db90617",
+    "url": "/educational-game-project-team-11-david-and-kevin/finalassets/images/pond.png"
+  },
+  {
+    "revision": "1189e723e82fdbad00f278b2968f2abc",
+    "url": "/educational-game-project-team-11-david-and-kevin/finalassets/images/tiles01.png"
+  },
+  {
+    "revision": "f258441fa8f9aa5929b5233fd327cf9b",
+    "url": "/educational-game-project-team-11-david-and-kevin/finalassets/images/tiles11.png"
+  },
+  {
+    "revision": "cf053af14504ca694cc78b106f9def53",
+    "url": "/educational-game-project-team-11-david-and-kevin/finalassets/images/updatebackground.png"
+  },
+  {
+    "revision": "dcdd1bd9377c2c80a170c42207c4e905",
+    "url": "/educational-game-project-team-11-david-and-kevin/finalassets/images/updatebackground.xcf"
+  },
+  {
+    "revision": "426c64762368c2e78f65f511ad0edbb8",
+    "url": "/educational-game-project-team-11-david-and-kevin/finalassets/spritesheets/AE.png"
+  },
+  {
+    "revision": "04b302c3f89bc0a44ddc579ce354926f",
+    "url": "/educational-game-project-team-11-david-and-kevin/finalassets/spritesheets/AE_sprites.png"
+  },
+  {
+    "revision": "1e27e93bcb2fd9b82661b8054e31a700",
+    "url": "/educational-game-project-team-11-david-and-kevin/finalassets/spritesheets/BasicMapTiles.png"
+  },
+  {
+    "revision": "96e4d0c0283eba0eee45713c1cbcd786",
+    "url": "/educational-game-project-team-11-david-and-kevin/finalassets/spritesheets/CS.png"
+  },
+  {
+    "revision": "342e479a3b641d829d5cdd544c70633c",
+    "url": "/educational-game-project-team-11-david-and-kevin/finalassets/spritesheets/CS_sprites.png"
+  },
+  {
+    "revision": "374ee56ae71305e036e5f5929be2e8ee",
+    "url": "/educational-game-project-team-11-david-and-kevin/finalassets/spritesheets/forrestTile.png"
+  },
+  {
+    "revision": "078e019c7324160e42074f7b26c3a011",
+    "url": "/educational-game-project-team-11-david-and-kevin/finalassets/spritesheets/icons.png"
+  },
+  {
+    "revision": "743745d7bb43111c304e486adfb81afa",
+    "url": "/educational-game-project-team-11-david-and-kevin/finalassets/spritesheets/seedsandplants.png"
+  },
+  {
+    "revision": "28a79e97337144fe01ea5039e4afc5ce",
+    "url": "/educational-game-project-team-11-david-and-kevin/finalassets/spritesheets/tubing.png"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "/educational-game-project-team-11-david-and-kevin/finalfavicon.ico"
+  },
+  {
+    "revision": "212219490e6f053c3f03",
+    "url": "/educational-game-project-team-11-david-and-kevin/finalgen.main.39ea56272c9ea712cd23.bundle.js"
+  },
+  {
+    "revision": "5e69403aa42e950fedff",
+    "url": "/educational-game-project-team-11-david-and-kevin/finalgen.vendors.0805bb80903235d40eaf.bundle.js"
+  },
+  {
+    "revision": "37c2dbf1b0f269ba3935b34073ecfcd4",
+    "url": "/educational-game-project-team-11-david-and-kevin/finalindex.html"
+  },
+  {
+    "revision": "2820266d63e93bb9239043f6277350bf",
+    "url": "/educational-game-project-team-11-david-and-kevin/finalmanifest.json"
+  },
+  {
+    "revision": "a8029749c7edd38204cf67287cb14139",
+    "url": "/educational-game-project-team-11-david-and-kevin/finalsw.js"
+  }
+]);
---
+++ final/sw.js
@@ -0,0 +1,11 @@
+importScripts("/educational-game-project-team-11-david-and-kevin/finalprecache-manifest.44ae7d5615360fb1a8d61346f35a4b8c.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/**
+ * You should only modify this, if you know what you are doing.
+ * This phaser template is using workbox (https://developers.google.com/web/tools/workbox/)
+ * to precache all assets.
+ * It uses the InjectManifest function from 'workbox-webpack-plugin' inside
+ * webpack/webpack.common.js
+ */
+workbox.precaching.precacheAndRoute(__precacheManifest)
+
